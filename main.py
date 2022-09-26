08b5cc727e1a4fc7351f35b7e80fa07c7bb4eed0
Lailz <lailaabdulraheem@gmail.com>
2022-09-26 16:27:55 +0300

    add prints

--- main.py
+++ main.py
@@ -3,25 +3,35 @@
 
 # Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
 
+
 def change_age(person, number):
     ...
 
 
+print(change_age(person, 21))
+
+
 # Create a function called `add_hobby` which takes the `person` dictionary and a `hobby` as arguments. This function adds the hobby to the list of `hobbies` inside the person dictionary. The function will return the updated dictionary.
+
 
 def add_hobby(person, hobby):
     ...
 
+
+print(add_hobby(person, "Table Tennis"))
 
 
 # CHALLENGE
 
 # Create a function called `add_job` which takes `person` dictionary and `job` which is a string, as arguments. This function checks, if the dictionary does not have a property called `job` then it will add it to the dictionary. The function will return the updated dictionary.
 
+
 def add_job(person, job):
     ...
 
+
 # 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. This function checks the number of hobbies for this person, if it's more than three hobbies, print to the user a message telling them that they're talented.
+
 
 def check_hobbies(person):
     ...
