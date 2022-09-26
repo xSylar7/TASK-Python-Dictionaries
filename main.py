647cdc320622f6fa3b4bf8dbab6355b3c24e5341
Laila AlKandery <laila.a@joincoded.com>
2022-09-26 16:16:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# 1. Create a dictionary called `person`, it has three properties, `name` which is a string, `age` which is an integer, `hobbies` which is a list of strings.
+# 2. Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
+# 3. Create a function called `add_hobby` which takes the `person` dictionary and a `hobby` as arguments. This function adds the hobby to the list of `hobbies` inside the person dictionary. The function will return the updated dictionary.
+
+# ## Challenge
+# 1. Create a function called `add_job` which takes a `job` which is a string, as an argument. This function checks, if the dictionary does not have a property called `job` then it will add it to the dictionary. The function will return the updated dictionary.
+# 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. This function checks the number of hobbies for this person, if it's more than three hobbies, print to the user a message telling them that they're talented.
